0a464a352218a30f4addd76fa78e388209b64901
Manuel Olivares <manu.olivares2@gmail.com>
2021-03-05 18:43:10 +0100

    Ultimos cambios

---
+++ src/app/shared/sidebar/sidebar.component.ts
@@ -0,0 +1,23 @@
+import { Component, OnInit } from '@angular/core';
+import { GifsService } from '../../gifs/services/gifs.service';
+
+@Component({
+  selector: 'app-sidebar',
+  templateUrl: './sidebar.component.html',
+  styleUrls: ['./sidebar.component.css']
+})
+export class SidebarComponent implements OnInit {
+
+  get historial() {
+    return this.gifsService.historial;
+  }
+
+  buscar(termino: string) {
+    this.gifsService.buscarGifs(termino);
+  }
+  constructor( private gifsService: GifsService ) { }
+
+  ngOnInit(): void {
+  }
+
+}
